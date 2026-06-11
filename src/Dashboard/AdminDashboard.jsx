de6a1af49ebc231c18d8fc748f072d56fc5a2d0e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-06-11 20:31:13 +0600

    added

--- src/Dashboard/AdminDashboard.jsx
+++ src/Dashboard/AdminDashboard.jsx
@@ -4,6 +4,7 @@ const AdminDashboard = () => {
     return (
         <div className='h-screen flex items-center justify-center text-2xl font-bold text-black'>
             This is Dashboard. Only admin can access this page. If you are not admin, please go back to home page.
+            
         </div>
     );
 };
